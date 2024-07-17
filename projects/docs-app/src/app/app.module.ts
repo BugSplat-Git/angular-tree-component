ee6e405334d4ca624b6e4b01cb9b3b2968bc4c9f
Bobby Galli <bobbyg603@pm.me>
2024-07-17 17:34:39 -0400

    chore: update MatButtonModule

--- projects/docs-app/src/app/app.module.ts
+++ projects/docs-app/src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { GettingStartedComponent } from './getting-started/getting-started.component';
-import { AppRoutingModule } from './app-routing.module';
-import { CustomElementsModule } from './custom-elements/custom-elements.module';
-import { CodeExampleModule } from './custom-elements/code/code-example.module';
-import { ExamplesModule } from './examples/examples.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TreeModule } from 'angular-tree-component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CodeExampleModule } from './custom-elements/code/code-example.module';
+import { CustomElementsModule } from './custom-elements/custom-elements.module';
+import { ExamplesModule } from './examples/examples.module';
 import { FundamentalsModule } from './fundamentals/fundamentals.module';
+import { GettingStartedComponent } from './getting-started/getting-started.component';
+import { GuidesModule } from './guides/guides.module';
 import { LayoutModule } from './layout/layout.module';
 import { SharedModule } from './shared/shared.module';
-import { GuidesModule } from './guides/guides.module';
 
-@NgModule({ declarations: [AppComponent, GettingStartedComponent],
+@NgModule({
+    declarations: [AppComponent, GettingStartedComponent],
     bootstrap: [AppComponent], imports: [SharedModule,
         BrowserModule,
         BrowserAnimationsModule,
@@ -34,5 +34,6 @@ import { GuidesModule } from './guides/guides.module';
         ExamplesModule,
         TreeModule,
         FundamentalsModule,
-        GuidesModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
-export class AppModule {}
+        GuidesModule], providers: [provideHttpClient(withInterceptorsFromDi())]
+})
+export class AppModule { }
